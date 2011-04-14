4f5a3cb6d159ad122b3f0336b0d09975a18957a5
Evan Dorn <evan@lrdesign.com>
2011-04-13 17:28:28 -0700

    moved more stuff around for pre-gemification

--- init.rb
+++
@@ -1,5 +0,0 @@
-# require File.join(File.dirname(__FILE__), "lib", "lrd_view_helper")  
-# require File.join(File.dirname(__FILE__), "lib", "lrd_debug_helper")  
-# ActionView::Base.send :include, LRD::ViewHelper  
-# ActionView::Base.send :include, LRD::DebugHelper  
-require 'lrd_view_tools'
