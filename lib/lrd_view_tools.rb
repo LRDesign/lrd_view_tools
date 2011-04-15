cec7e0b7b9ff4a0e4040844d0d8bce55317470e0
Evan Dorn <evan@lrdesign.com>
2011-04-15 13:58:29 -0700

    cleaned up some unused stuff

--- lib/lrd_view_tools.rb
+++ lib/lrd_view_tools.rb
@@ -1,5 +1,11 @@
-require File.join(File.dirname(__FILE__), '..', 'app', 'helpers', 'lrd_view_helper')
-require File.join(File.dirname(__FILE__), '..', 'app', 'helpers', 'lrd_debug_helper')
+require File.join(File.dirname(__FILE__), 'app', 'helpers', 'lrd_view_helper')
+require File.join(File.dirname(__FILE__), 'app', 'helpers', 'lrd_debug_helper')
 
-ActionController::Base.helper(LRD::ViewHelper)
-ActionController::Base.helper(LRD::DebugHelper)
+module LRD::DevTools
+  class Railtie < Rails::Railtie
+    ActionController::Base.helper(LRD::ViewHelper)
+    ActionController::Base.helper(LRD::DebugHelper)
+  end
+end
+
+
