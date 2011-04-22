45d2577e14cc8425ac37718c57ba2081f32b75c4
Evan Dorn <evan@lrdesign.com>
2011-04-22 16:22:30 -0700

    form helper is in flux

--- lib/app/helpers/lrd_view_helper.rb
+++ lib/app/helpers/lrd_view_helper.rb
@@ -37,46 +37,6 @@ module LRD
       block_to_partial('shared/block', options.merge(:title => title), &block).html_safe
     end
 
-
-    # pass { :nolabel => true } to replace the label with a spacer
-    # pass { :required => true } to dispay as a required field
-    # pass { :text => "foo" } to override the label text
-    # pass { :class => 'foo'} to add 'foo' to the CSS class of the <div>
-    #
-    # input_options hash gets passed directly to the input field
-    def labeled_input(form, field, options = {}, input_options = {})
-      options[:text] = "&nbsp;".html_safe if options[:nolabel]
-      options.reverse_merge!(:text => nil,:required => false, :nolabel => false)
-      options.merge!(:form => form, :field => field)
-      input_options.reverse_merge!( :size => 30 )
-
-      cssclass = "labeled_input"
-      cssclass += " required" if options[:required]
-      cssclass += " #{options[:class]}" if options[:class]
-
-      unless input = options[:input]
-        input = form.text_field field, input_options
-      end
-
-      if field.blank?
-        label = (content_tag :label, options[:text]).html_safe
-      else
-        label = (form.label field, options[:text]).html_safe
-      end
-      comment = options[:comment] ? content_tag( :span, { :class => 'comment' } ) { options[:comment] }  : ""
-
-      content_tag(:div, (label + input + comment), { :class => cssclass }).html_safe
-    end
-
-    # creates a submit button that lines up with a bunch of labeled_input fields
-    def unlabeled_submit(form, text = nil)
-      if text
-        labeled_input(form, nil, :input => form.submit(text), :nolabel => true).html_safe
-      else
-        labeled_input(form, nil, :input => form.submit, :nolabel => true).html_safe
-      end
-    end
-
   end
 
   def debug_link(name)
--- lib/lrd_view_tools.rb
+++ lib/lrd_view_tools.rb
@@ -1,5 +1,16 @@
-require File.join(File.dirname(__FILE__), '..', 'app', 'helpers', 'lrd_view_helper')
-require File.join(File.dirname(__FILE__), '..', 'app', 'helpers', 'lrd_debug_helper')
+module LRD
+  module ViewTools
 
-ActionController::Base.helper(LRD::ViewHelper)
-ActionController::Base.helper(LRD::DebugHelper)
+    class Railtie < Rails::Railtie
+
+      require File.expand_path('../app/helpers/lrd_view_helper', __FILE__)
+      require File.expand_path('../app/helpers/lrd_form_helper', __FILE__)
+      require File.expand_path('../app/helpers/lrd_debug_helper', __FILE__)
+
+      ActionController::Base.helper(LRD::ViewHelper)
+      ActionController::Base.helper(LRD::DebugHelper)
+      ActionView::Helpers::FormHelper.send(:include, LRD::FormHelper)
+
+    end
+  end
+end
